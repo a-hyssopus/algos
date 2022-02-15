d1d11a7bea49a9dc83ce08b91141b37b1e1edf71
Daria Balutel <ceban.daria2000@gmail.com>
2022-02-15 17:46:37 +0100

    Repeating Symbols

---
+++ repeatingSymbols.js
@@ -0,0 +1,17 @@
+const repeatingSymbols = (string) => {
+    let symbols = new Map();
+
+    for (let symbol of string) {
+        if (symbols.has(symbol)) {
+            symbols.set(symbol, symbols.get(symbol) + 1)
+        } else {
+            symbols.set(symbol, 1)
+        }
+    }
+
+    return [...symbols].filter(([key, value]) => value > 1).map(([key]) => key);
+}
+
+repeatingSymbols(['c', 'a', 'i', 'o', 'p', 'a', 'p', 'p', 'o', 'j'])
+
+// the complexity of this algorithm equals to O(n) for both time and space, where n is the length of string the function receives
